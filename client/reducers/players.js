379d6e885d4f3c7e149fe5074f4aeb10e3ed85e7
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-10-06 08:46:50 -0400

    FEAT(redux): added players sub reducer and action creators

---
+++ client/reducers/players.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+
+const initialState = {
+  players: [],
+  singlePlayer: {}
+}
+
+/**********PLAYERS CREATORS**********/
+
+const GET_PLAYERS = 'GET_PLAYERS'
+
+export const getPlayersActionCreator = players => {
+  type: GET_PLAYERS,
+    players
+}
+
+export const getPlayersThunkCreator = () => {
+  return async dispatch => {
+    try {
+      const { data } = await axios.get('/api/players')
+      dispatch(getPlayersActionCreator(data)))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+/************PLAYERS SUB-REDUCER*************/
+
+export const playersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_PLAYERS:
+      return { ...state, players: action.players }
+    default:
+      return state
+  }
+}
