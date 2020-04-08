601ad42b4ed064c46a8f8c6ca293507e6a5ffb43
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2020-04-08 11:15:47 -0400

    removed redux functionality from setup

--- client/components/Setup/Setup.js
+++ client/components/Setup/Setup.js
@@ -1,24 +1,50 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import axios from 'axios'
+// import { connect } from 'react-redux'
 
-import { getPlayersThunkCreator, addPlayerThunkCreator, deletePlayerThunkCreator } from '../../reducers/players'
-import { updateHighestRoundThunkCreator, getHighestRoundThunkCreator } from '../../reducers/game'
+// import { getPlayersThunkCreator, addPlayerThunkCreator, deletePlayerThunkCreator } from '../../reducers/players'
+// import { updateHighestRoundThunkCreator, getHighestRoundThunkCreator } from '../../reducers/game'
 
-class DisconnectedSetup extends Component {
+export class Setup extends Component {
   constructor(props) {
     super(props)
-    this.onSubmit = this.onSubmit.bind(this)
+    this.state = {
+      players: [],
+      currRound: 0,
+      tricksTaken: 0
+    }
+    this.addPlayer = this.addPlayer.bind(this)
+    this.deletePlayer = this.deletePlayer.bind(this)
   }
-  onSubmit(event) {
+  async addPlayer(event) {
     event.preventDefault()
-    this.props.addingPlayer(event.target.name.value)
+    event.persist()
+    try {
+      const { data } = await axios.post('/api/players', { name: event.target.name.value })
+      this.setState({ players: [...this.state.players, data] })
+    } catch (error) {
+      console.log(error)
+    }
     event.target.name.value = ''
   }
-  componentDidMount() {
-    this.props.gettingPlayers()
+  async deletePlayer(id) {
+    try {
+      const { data } = await axios.delete(`/api/players/${id}`)
+      this.setState({ players: this.state.players.filter(player => { return player.id !== id }) })
+    } catch (error) {
+      console.log(error)
+    }
+  }
+  async componentDidMount() {
+    try {
+      const { data } = await axios.get('/api/players')
+      this.setState({ players: data })
+    } catch (error) {
+      console.log(error)
+    }
   }
   render() {
-    let numPlayers = this.props.players.length
+    let numPlayers = this.state.players.length
     let maxCards = numPlayers <= 5 ? 10 : (52 - (52 % numPlayers)) / numPlayers
 
     return (
@@ -27,19 +53,19 @@ class DisconnectedSetup extends Component {
         <div className="players-and-add-player">
           <div>
             <p>Players:</p>
-            {this.props.players.map(player => {
+            {this.state.players.map(player => {
               return (
                 <div key={player.id}>
                   <p>{player.name}</p>
                   <button type="button" onClick={() => {
-                    this.props.deletingPlayer(player.id)
+                    this.deletePlayer(player.id)
                   }}>Delete player</button>
                 </div>
               )
             })}
           </div>
           <div>
-            <form onSubmit={this.onSubmit}>
+            <form onSubmit={(event) => this.addPlayer(event)}>
               <p>Add player:</p>
               <input type="text" className="nameInput" name="name" />
               <input type="submit" className="nameSubmit" name="submit" />
@@ -52,22 +78,7 @@ class DisconnectedSetup extends Component {
         <div>
           <button type="button">Start Game!</button>
         </div>
-      </div>
+      </div >
     )
   }
 }
-
-const mapStateToProps = state => {
-  return {
-    players: state.playersReducer.players,
-    highestRound: state.gameReducer.highestRound
-  }
-}
-
-const mapDispatchToProps = dispatch => ({
-  gettingPlayers: () => dispatch(getPlayersThunkCreator()),
-  addingPlayer: (name) => dispatch(addPlayerThunkCreator(name)),
-  deletingPlayer: (id) => dispatch(deletePlayerThunkCreator(id))
-})
-
-export const Setup = connect(mapStateToProps, mapDispatchToProps)(DisconnectedSetup)
--- client/main.js
+++ client/main.js
@@ -2,23 +2,23 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Switch, Route, Redirect } from 'react-router'
 import { BrowserRouter, Link } from 'react-router-dom'
-import { Provider } from 'react-redux'
+// import { Provider } from 'react-redux'
 
-import store from './store'
+// import store from './store'
 import { Root } from './components/root/root'
 import { Setup } from './components/Setup/Setup'
 
 render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <div className="bg">
-        <Switch>
-          <Route exact path="/" component={Root} />
-          <Route exact path="/setup" component={Setup} />
-          {/* <Route exact path="/scores" component={Scores} /> */}
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>,
+  // <Provider store={store}>
+  <BrowserRouter>
+    <div className="bg">
+      <Switch>
+        <Route exact path="/game" component={Root} />
+        {/* <Route exact path="/" component={Setup} /> */}
+        {/* <Route exact path="/scores" component={Scores} /> */}
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  // </Provider>,
   document.getElementById('main')
 )
